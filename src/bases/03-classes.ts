09d01e384c2b855c66bbae34142e5b2771af910a
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-10-20 01:10:58 -0400

    readonly, getters, methods and this

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,8 +1,26 @@
 export class Pokemon {
 
+    get image_url(): string {
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
+
     constructor(
-        public id: number,
-        public name: string) { }
+        public readonly id: number,
+        public name: string,
+    ) { }
+
+    scream() {
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+    speak() {
+        console.log(`${this.name}, ${this.name}`);
+
+    }
+
 }
 
 export const charmander = new Pokemon(1, 'Charmander');
+
+console.log(charmander.image_url);
+charmander.scream();
+charmander.speak();
